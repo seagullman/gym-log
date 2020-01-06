f392d472f895b468f06be5a900fc905150aa13fd
Brad Siegel <brad.siegel@spr.com>
2020-01-06 11:36:42 -0600

    Initial Commit

---
+++ gym-log/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gym-log
+//
+//  Created by Brad Siegel on 1/6/20.
+//  Copyright © 2020 Seagull LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
